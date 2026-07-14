62dd49734edeb897f4107a79e1852da67a07ca63
mihirgajjar2810 <mihirgajjar2810@gmail.com>
2026-07-14 12:33:35 +0530

    upadated search bar

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
+from django.db.models import Q
 
 def product_list(request, category_slug=None):
 
@@ -10,7 +11,9 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-
+    query = request.GET.get('q')
+    if query:
+        products = products.filter( Q (name__icontains=query) | Q(description__icontains=query) )
     return render(request, 'products/product_list.html', {
         'category': category,
         'categories': categories,
